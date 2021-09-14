d96e07674012d9efebcef37cf1fcb74e30b5a8e3
Aktam <abdunazarov9798@gmail.com>
2021-09-14 14:03:18 +0500

    aSdfghj

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -7,5 +7,7 @@ namespace MInnovation.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public int Error { get; set; }
     }
 }
